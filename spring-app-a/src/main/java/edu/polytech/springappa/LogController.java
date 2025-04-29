04b749102625a0442aa553cea42ea7ce2e24a341
HanineHassayoune <hassayounehanine@gmail.com>
2025-04-29 21:25:44 +0200

    commit filter error

--- spring-app-a/src/main/java/edu/polytech/springappa/LogController.java
+++ spring-app-a/src/main/java/edu/polytech/springappa/LogController.java
@@ -17,10 +17,10 @@ public class LogController {
     @GetMapping("/info")
     public String infoLog() throws JsonProcessingException {
         LogModel logModel = new LogModel("Ceci est un log INFO depuis Spring Boot");
-        ObjectMapper objectMapper = new ObjectMapper();
-        String log = objectMapper.writeValueAsString(logModel);
+        //ObjectMapper objectMapper = new ObjectMapper();
+        //String log = objectMapper.writeValueAsString(logModel);
 
-        logger.info(log);
+        logger.info("{}",logModel);
         return "Log INFO généré";
     }
 
@@ -37,10 +37,10 @@ public class LogController {
     @GetMapping("/warn")
     public String warnLog() throws JsonProcessingException {
         LogModel logModel = new LogModel("Ceci est un log WARN depuis Spring Boot");
-        ObjectMapper objectMapper = new ObjectMapper();
-        String log = objectMapper.writeValueAsString(logModel);
+        //ObjectMapper objectMapper = new ObjectMapper();
+        //String log = objectMapper.writeValueAsString(logModel);
 
-        logger.warn(log);
+        logger.warn("{}",logModel);
         return "Log WARN généré";
     }
 }
