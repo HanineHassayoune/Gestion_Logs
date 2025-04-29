7ea100c89ab8d56273d9edc6e31f3dbd5df62367
HanineHassayoune <hassayounehanine@gmail.com>
2025-04-29 18:00:15 +0200

    commit 2

--- spring-app-a/src/main/java/edu/polytech/springappa/LogController.java
+++ spring-app-a/src/main/java/edu/polytech/springappa/LogController.java
@@ -1,9 +1,12 @@
 package edu.polytech.springappa;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 
 @RestController
 @RequestMapping("/api/logs")
@@ -12,20 +15,32 @@ public class LogController {
     private static final Logger logger = LoggerFactory.getLogger(LogController.class);
 
     @GetMapping("/info")
-    public String infoLog() {
-        logger.info("Ceci est un log INFO depuis Spring Boot.");
+    public String infoLog() throws JsonProcessingException {
+        LogModel logModel = new LogModel("Ceci est un log INFO depuis Spring Boot");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String log = objectMapper.writeValueAsString(logModel);
+
+        logger.info(log);
         return "Log INFO généré";
     }
 
     @GetMapping("/error")
-    public String errorLog() {
-        logger.error("Ceci est un log ERROR depuis Spring Boot.");
+    public String errorLog() throws JsonProcessingException {
+        LogModel logModel = new LogModel("Ceci est un log ERROR depuis Spring Boot");
+        //ObjectMapper objectMapper = new ObjectMapper();
+        //String log = objectMapper.writeValueAsString(logModel);
+
+        logger.error("{}",logModel);
         return "Log ERROR généré";
     }
 
     @GetMapping("/warn")
-    public String warnLog() {
-        logger.warn("Ceci est un log WARN depuis Spring Boot.");
+    public String warnLog() throws JsonProcessingException {
+        LogModel logModel = new LogModel("Ceci est un log WARN depuis Spring Boot");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String log = objectMapper.writeValueAsString(logModel);
+
+        logger.warn(log);
         return "Log WARN généré";
     }
 }
