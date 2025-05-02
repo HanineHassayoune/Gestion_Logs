c5f0321b663c38813f9327dc3ecfcb4bf3099993
HanineHassayoune <hassayounehanine@gmail.com>
2025-05-02 22:49:26 +0200

    commit send data with http

--- spring-app-a/src/main/java/edu/polytech/springappa/LogControllerr.java
+++ spring-app-a/src/main/java/edu/polytech/springappa/LogControllerr.java
@@ -5,14 +5,13 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 
 @RestController
 @RequestMapping("/api/logs")
-public class LogController {
+public class LogControllerr {
 
-    private static final Logger logger = LoggerFactory.getLogger(LogController.class);
+    private static final Logger logger = LoggerFactory.getLogger(LogControllerr.class);
 
     @GetMapping("/info")
     public String infoLog() throws JsonProcessingException {
